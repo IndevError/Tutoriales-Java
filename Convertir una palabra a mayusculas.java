3f226c1c58b0bafd499ad72f1400759b005023ab
Indev-Error <86646025+IndevError@users.noreply.github.com>
2021-12-02 13:08:30 -0600

    Add files via upload

---
+++ Convertir una palabra a mayusculas.java
@@ -0,0 +1,20 @@
+//Libreria
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String Palabra;
+		Scanner teclado = new Scanner(System.in);
+		
+		//Obtenemos la palabra
+		System.out.println("Ingresa una palabra:");
+		Palabra = teclado.nextLine();
+		
+		//Se cambia toda la palabra a mayusculas
+        Palabra = Palabra.toUpperCase();
+
+		//Imprimimos la palabra sin espacios
+		System.out.println("\n--> "+Palabra);
+	}
+}
